d295961ce88e84122c92e990bb4faff8d58c28c1
Genelle Geerman <g.geerman@student.fontys.nl>
2024-04-28 01:25:51 +0200

    Added basic save func

--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 signal menu_closed()
 
-
-
 func _on_return_pressed():
 	menu_closed.emit();
 	self.hide();
--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -1,10 +1,17 @@
-extends Node
+extends Node2D
 
 @onready var mainScreen = get_node("Main Screen");
 @onready var settingsMenu = get_node("SettingsMenu");
 
+func _ready():
+	var hasSaveData = Utils.load_game();
+	if(!hasSaveData):
+		var continueButton = get_node("Main Screen/Continue");
+		continueButton.disabled = true;
+		continueButton.focus_mode = Button.FOCUS_NONE;
+		
 func _on_new_game_pressed():
-	pass
+	get_tree().change_scene_to_file("res://Scenes/start_scene.tscn")
 
 func _on_continue_pressed():
 	pass
