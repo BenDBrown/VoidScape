7fe629b6323e22c23ce8015578936f4c08df4545
Genelle Geerman <g.geerman@student.fontys.nl>
2024-05-03 14:51:56 +0200

    Revert "adjusted gitignore and ship saver."
    This reverts commit 54ebc067e28f544d90db4385757fec8253ade0ef.

--- Scripts/Game/player_spawner.gd
+++ Scripts/Game/player_spawner.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@onready var parent = $Node2D
-var saver: ShipSaver = ShipSaver.new()
+@export var saver:ShipSaver = preload("res://Scripts/Resources/player_ship_saver.gd")
 
+@onready var parent = $Node2D
 func _ready():
 	saver.create(parent)
