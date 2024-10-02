0b266a70e63ff7e1cc6f6f7941f91ce2afe75ed2
Benjamin Brown <benjamin.d.brown.2002@gmail.com>
2024-10-02 23:11:19 +0200

    rotation on axis

--- Global/quest_manager.gd
+++ Global/quest_manager.gd
@@ -4,14 +4,14 @@ extends Node
 @export var quests = []
 
 func try_add_quest(quest):
-    if quests.size() >= questLimit:
-        return false
-    quests.append(quest)
-    return true
+	if quests.size() >= questLimit:
+		return false
+	quests.append(quest)
+	return true
 
 func get_quests():
-    return quests
+	return quests
 
 func check(target):
-    for quest in quests:
-        quest.check_target(target)
+	for quest in quests:
+		quest.check_target(target)
