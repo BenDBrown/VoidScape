3f421a913709790cc118a01eb7872ee6b268775a
GabiHr <g.hristova@student.fontys.nl>
2024-11-06 02:30:21 +0100

    added explosions sprites

--- UI/Scenes/Components/Component.cs
+++ UI/Scenes/Components/Component.cs
@@ -6,17 +6,11 @@ public partial class Component : Control
 	[Export]
 	public ShipComponentData DataResource;
 
-	[Export]
 	public TextureRect texture;
-	
+
 	public override void _Ready()
 	{
 		texture.Texture = DataResource.Sprite;
-
 	}
 
-	
-	public override void _Process(double delta)
-	{
-	}
 }
