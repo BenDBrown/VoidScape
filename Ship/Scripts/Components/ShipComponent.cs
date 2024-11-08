e062e6c3b7789fc1d44069a91687d9f73e748ff3
BenDBrown <benjamin.d.brown.2002@gmail.com>
2024-11-08 11:51:20 +0100

    collider parent fix

--- Ship/Scripts/Components/ShipComponent.cs
+++ Ship/Scripts/Components/ShipComponent.cs
@@ -10,7 +10,7 @@ public partial class ShipComponent : CharacterBody2D
     public delegate void OnDestroyedEventHandler(ShipComponent shipComponent);
 
     [Export]
-    private CollisionShape2D collider;
+    public CollisionShape2D collider { get; private set; }
     [Export]
     private Node2D[] vertices;
 
