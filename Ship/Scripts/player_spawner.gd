61ce5e08551581c664c163dc0ae14a909db452e9
Genelle Geerman <g.geerman@student.fontys.nl>
2024-12-02 11:54:02 +0100

    Made a HUD for the player

--- Ship/Scripts/player_spawner.gd
+++ Ship/Scripts/player_spawner.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 @export var player_ship_scene: PackedScene = preload("res://Ship/Prefabs/PlayerShip.tscn")
+@export var hud_scene: PackedScene = preload("res://UI/Scenes/HUD.tscn")
+
+var saver
 
 func _ready():
-	var saver = PlayerShipSave.new()
+	saver = PlayerShipSave.new()
 	saver = saver.load_save()
-
 	var playerShip = player_ship_scene.instantiate() as Ship
-	get_parent().add_child.call_deferred(playerShip)
+	Game.PlayerShip = playerShip
+	call_deferred("deferred", playerShip);
+
+func deferred(playerShip):
+	var hud = hud_scene.instantiate()
+	get_parent().add_child(playerShip)
+	get_parent().add_child(hud)
 	saver.build_ship(playerShip)
 	playerShip.TryBuildShip()
 	playerShip.global_position = global_position
-	Game.PlayerShip = playerShip
