00ef855fb909e0e7c22c4d7ecc6b2a7b1c96c90a
Genelle Geerman <g.geerman@student.fontys.nl>
2025-01-07 23:13:38 +0100

    fixed location and removed old drop comp

--- Components/drop_component.gd
+++ Components/drop_component.gd
@@ -2,6 +2,6 @@ extends Node
 
 @export var cargo: Cargo
 
-func _on_parent_destroyed(ship: CharacterBody2D) -> void:
+func drop(ship: CharacterBody2D) -> void:
 	if cargo:
 		cargo.Spawn(get_tree().current_scene, ship.global_position)
--- Components/loot_drop_component.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-class_name ItemDropComponent
-@export var drops: Array[Cargo]
-
-func drop_loot():
-	for loot in drops:
-		loot.call_deferred("spawn", get_tree().current_scene, global_position)
