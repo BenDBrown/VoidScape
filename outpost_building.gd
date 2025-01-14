ade8d1af39ff0f52f917e4e1ac4e06794d36a998
MelodyTheMuse <105441849+MelodyTheMuse@users.noreply.github.com>
2025-01-14 21:10:37 +0100

    Update outpost_building.gd

--- outpost_building.gd
+++ outpost_building.gd
@@ -26,6 +26,7 @@ func _on_health_component_died() -> void:
 	respawn_timer.queue_free()
 
 func create_enemy():
+	#WHAT THE FUCK IS SPAMMING YOU, YOU ASSHOLE STOP IT 
 	grunt = grunt_scene.instantiate()
 	grunts_alive += 1
 	grunt.connect("OnDestroyed",on_enemy_death)
@@ -42,6 +43,7 @@ func start_respawn_timer_enemy():
 				if respawn_timer.time_left == 0:
 					print("why are toy here")
 					respawn_timer.start(respawn_time)
+					
 
 func first_enemy_wave():
 	respawn_timer = Timer.new()
