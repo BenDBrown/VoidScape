e577f16e7e10784dd9b98bf5a81f79abdaeb9f60
Benjamin Brown <b.brown@student.fontys.nl>
2025-01-23 00:34:52 +0100

    tile map adjusted for lvl merge and added spawn point

--- Ship/Scripts/player_spawner.gd
+++ Ship/Scripts/player_spawner.gd
@@ -30,9 +30,18 @@ func deferred(playerShip:PlayerShip):
 	get_parent().add_child(inventory)
 	saver.build_ship(playerShip)
 	playerShip.TryBuildShip()
-	playerShip.global_position = global_position
+	set_spawn(playerShip)
 	playerShip.OnDestroyed.connect(on_destroyed)
 
 func on_destroyed(ship):
 	hud.hide()
 	gameOver.show()
+	
+func set_spawn(ship: PlayerShip):
+	var spawnPointSave = SpawnPointSave.new()
+	spawnPointSave = spawnPointSave.load_save()
+	if(spawnPointSave.spawn_point != Vector2.ZERO):
+		ship.global_position = spawnPointSave.spawn_point
+	else: 
+		ship.global_position = global_position
+	
