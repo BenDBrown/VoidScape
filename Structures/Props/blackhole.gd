bf63480be84c5312470fe4a177be4e6d5989c0a0
Genelle Geerman <g.geerman@student.fontys.nl>
2025-01-23 03:19:37 +0100

    made pull exported

--- Structures/Props/blackhole.gd
+++ Structures/Props/blackhole.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 var ships = []
-var pull = 12000
+@export var pull = 12000
 
 func _physics_process(delta: float) -> void:
 	for ship in ships:
 		var dist = ship.global_position.distance_to(global_position)
 		dist = abs(dist)
-		ship.AddExternalImpulse(ship.global_position.direction_to(global_position) * pull * 1/dist)
+		ship.AddExternalImpulse(ship.global_position.direction_to(global_position) * pull * 1 / dist)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
