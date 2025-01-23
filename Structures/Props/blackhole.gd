a05fb98bc4bb0e3a70d0096894ecbbe12b52d478
Genelle Geerman <g.geerman@student.fontys.nl>
2025-01-23 03:19:00 +0100

    Added Black Hole as a prefab

---
+++ Structures/Props/blackhole.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var ships = []
+var pull = 12000
+
+func _physics_process(delta: float) -> void:
+	for ship in ships:
+		var dist = ship.global_position.distance_to(global_position)
+		dist = abs(dist)
+		ship.AddExternalImpulse(ship.global_position.direction_to(global_position) * pull * 1/dist)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Ship:
+		ships.append(body)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Ship:
+		ships.erase(body)
